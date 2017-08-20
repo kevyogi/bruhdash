b1994a408375906fe5516d03eacae6c4eb8eedd5
Kevin Yogi <kevinyogi@Kevins-MacBook-Pro.local>
2017-08-19 14:34:38 -1000

    finished #initial exercise

--- js/bruh-dash.js
+++ js/bruh-dash.js
@@ -42,8 +42,13 @@ global.bruhdash = {
   },
 
   // returns an array with all elements except for the last element
-  initial: function () {
-
+  initial: function (arr) {
+    var newArray = [];
+    for(var i = 0; i < arr.length; i++){
+      if(i < arr.length - 1){
+        newArray.push(arr[i]);
+      }
+    }return newArray;
   },
   
   // returns an array with all falsey values removed
